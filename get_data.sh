7be0e30778d43fe25be5adfd3e92f957365194da
Lbexsis06 <benchikhnassim10@gmail.com>
2026-06-26 12:04:05 +0200

    chore: script de telechargement MovieLens

--- get_data.sh
+++ get_data.sh
@@ -2,17 +2,17 @@
 # Telecharge le jeu MovieLens (ml-latest-small) dans data/datasets/.
 set -e
 
-mkdir -p data/datasets
+mkdir -p data/raw
 
 echo "== Telechargement de MovieLens ml-latest-small =="
 curl -fSL https://files.grouplens.org/datasets/movielens/ml-latest-small.zip \
-  -o data/datasets/ml-latest-small.zip
+  -o data/raw/ml-latest-small.zip
 
 echo "== Decompression =="
-cd data/datasets
+cd data/raw
 unzip -o ml-latest-small.zip
 cd ../..
 
 echo ""
 echo "Termine. Contenu :"
-ls -lh data/datasets/ml-latest-small/
+ls -lh data/raw/ml-latest-small/
